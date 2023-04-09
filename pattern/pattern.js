c97587636e856be33e756b93f3091ce521568a5a
deepaksaini02 <15eiacs012@gmail.com>
2023-04-09 16:49:12 +0530

    pattern problems

---
+++ pattern/pattern.js
@@ -0,0 +1,77 @@
+
+const n=5
+
+/** output: 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+**/
+for(let i=0;i<n;i++){
+    let str=''
+    for(let j=0;j<n;j++){
+        str+='*'
+        if(j!=n)
+        str+=' '
+    }
+    console.log(str);
+}
+
+/** output: 
+*
+* *
+* * *
+* * * *
+* * * * *
+**/
+
+for(let i=0;i<n;i++){
+    let str=''
+    for(let j=0;j<i+1;j++){
+        str+='*'
+        if(j!=n)
+        str+=' '
+    }
+    console.log(str);
+}
+
+/** output: 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+**/
+
+for(let i=0;i<n;i++){
+    let str=''
+    for(let j=0;j<n-i;j++){
+        str+='*'
+        if(j!=n)
+        str+=' '
+    }
+    console.log(str);
+}
+
+/**
+ output:
+    *
+   ***
+  *****
+ *******
+*********
+
+ **/
+
+
+for(let i=0;i<n;i++){
+    let str=''
+    for(let j=0;j<n-i-1;j++){
+        str+=" "
+    }
+    for(let k=0;k<(2*i)+1;k++){
+        str+='*'
+    }
+    console.log(str);
+}
